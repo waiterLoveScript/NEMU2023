d7d22f718f1a544bdf6704a642b269707bd6bca4
tracer-NEMU2020 <tjuics2020@163.com>
2023-08-29 23:19:20 +0800

    >  compile NEMU 3022244347 lovescript Linux lovescript-virtual-linux 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:19:20 up  4:29,  1 user,  load average: 1.93, 1.20, 0.76 76a035bdb3224ede778cad39e24e4546791e946

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -179,4 +179,81 @@ WP* scan_watchpoint() {
 	return NULL;
 }
 
+/*WP* new_wp() {
+
+	if (free_ == NULL) return NULL;
+
+	WP *f_top, *h_tail;
+	f_top = free_;
+	h_tail = head;
+	free_ = free_ -> next;
+
+	f_top -> next = NULL;
+
+	if (h_tail == NULL) head = f_top;
+	else {
+		while (h_tail -> next != NULL)
+			h_tail = h_tail -> next;
+		h_tail -> next = f_top;
+	}
+	return f_top;
+}
+
+void free_wp(WP *wp) {
+
+	WP *h, *f;
+	h = head;
+	f = free_;
+	if (h == wp) {
+		head = wp -> next;
+	} else {
+		while (h != NULL && h -> next != wp) {
+			h = h -> next;
+		}
+		h -> next = h -> next -> next;
+	}
+	wp -> next = free_;
+	free_ = wp;
+	wp -> val = 0;
+	wp -> expr[0] = '\0';
+}
+
+void print_w() {
+	WP *h = head;
+	while (h != NULL) {
+		printf("[Watchpoint NO.%d]\tExpression: %s\tValue: %d\n", h -> NO, h -> expr, h -> val);
+		h = h -> next;
+	}
+}
+
+WP* delete_wp(int id, bool* f) {
+	WP* ret = head;
+	while (ret != NULL && ret -> NO != id) {
+		ret = ret -> next;
+	}
+	if (ret == NULL) *f = false;
+	return ret;
+}
+
+void delete_wp(int num)
+{
+	WP *f;
+	f = &wp_pool[num];
+	free_wp (f);
+}
+
+void check_wp(bool* f) {
+	WP* h = head;
+	while (h != NULL) {
+		bool tmp = true;
+		uint32_t nxtv = expr(h->expr, &tmp);
+		if (nxtv != h -> val) {
+			printf("[Watchpoint NO.%d]\tExpression: %s\tOrigin Value: 0x%x\tNew Value: 0x%x\n", h -> NO, h -> expr, h -> val, nxtv);
+			h -> val = nxtv;
+			*f = true;
+		}
+		h = h -> next;
+	}
+}*/
+
 
