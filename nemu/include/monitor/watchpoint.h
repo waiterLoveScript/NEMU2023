b22d33b35e0fd07b77de0c36cc51e25f370dc942
tracer-NEMU2020 <tjuics2020@163.com>
2023-08-29 21:34:14 +0800

    >  compile NEMU 3022244347 lovescript Linux lovescript-virtual-linux 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:34:14 up  2:44,  1 user,  load average: 0.97, 0.40, 0.35 aa0d5d9b60a648422c10d6bc7aa5e62a812d8b57

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -8,10 +8,16 @@ typedef struct watchpoint {
 	uint32_t val;
 	char expr [32];
 	struct watchpoint *next;
+	int b;
 
 	/* TODO: Add more members if necessary */
 
 
 } WP;
 
+WP* new_wp ();
+void free_wp(WP *);
+bool check_wp();
+void delete_wp(int );
+void info_wp();
 #endif
