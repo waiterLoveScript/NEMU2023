0e8a205b6c3e9fd8d89c29e9925300e27029e90f
Yixuan Qin <wait_er@tju.edu.cn>
2023-09-06 18:45:53 +0800

    PA2

--- nemu/src/cpu/exec/data-mov/leave-template.h
+++ nemu/src/cpu/exec/data-mov/leave-template.h
@@ -2,15 +2,13 @@
 
 #define instr leave
 
-static void do_execute() {
-    REG(R_ESP) = REG(R_EBP);
-    REG(R_EBP) = MEM_R(REG(R_ESP));
-    REG(R_ESP) += DATA_BYTE;
-	print_asm_template1();
+make_helper(leave){
+    cpu.esp=cpu.ebp;
+    /*only 32*/
+    cpu.ebp=MEM_R(cpu.esp);
+    cpu.esp+=4;
+    print_asm_template1();
+    return 1;
 }
-
-//#if DATA_BYTE == 4
-make_instr_helper(p)
-//#endif
 
 #include "cpu/exec/template-end.h"
